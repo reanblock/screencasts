46af2a410ae27c8aef8c7eb2f8b4a87504f3e76d
Darren Jensen <darren.jensen@gmail.com>
2023-10-24 15:59:31 +0700

    added freemem ptr and msize examples

--- 10-introduction-to-yul/yul-memory.sol
+++ 10-introduction-to-yul/yul-memory.sol
@@ -17,7 +17,16 @@ NOTE
 * Memory is arranged in 32 byte sequences
 * Only 4 instructions: mload, mstore, mstore8 and msize
 */
+
 contract YulMemory {
+    event FreeMemoryPointerAddress(bytes32 ptr);
+    event FreeMemoryPointerAddressMSize(bytes32 ptr, bytes32 _msize);
+
+    struct MemData {
+        uint256 n1;
+        uint256 n2;
+    }
+
     function mstoreExample() public pure {
         assembly {
             mstore(0x0, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
@@ -31,5 +40,48 @@ contract YulMemory {
             mstore8(0x0, 0x7)
             mstore(0x0, 0x7)
         }
+    }
+
+    // function demonstrates how solidity automatically 
+    // updates the free memory pointer
+    function freeMemoryPointer() public {
+        bytes32 ptr;
+        assembly {
+            ptr := mload(0x40)
+        }
+        emit FreeMemoryPointerAddress(ptr);
+        MemData memory data = MemData({n1: 1, n2: 2});
+        data.n1; // hides warning
+        assembly {
+            ptr := mload(0x40)
+        }
+        emit FreeMemoryPointerAddress(ptr);
+    }
+
+    // function demonstrates msize
+    function memSizeExample() public {
+        bytes32 ptr;
+        bytes32 _msize;
+        assembly {
+            ptr := mload(0x40)
+            _msize := msize()
+        }
+        emit FreeMemoryPointerAddressMSize(ptr, _msize);
+
+        MemData memory data = MemData({n1: 1, n2: 2});
+        data.n1; // hides warning
+
+        assembly {
+            ptr := mload(0x40)
+            _msize := msize()
+        }
+        emit FreeMemoryPointerAddressMSize(ptr, _msize);
+
+        assembly {
+            pop(mload(0xff))
+            ptr := mload(0x40)
+            _msize := msize()
+        }
+        emit FreeMemoryPointerAddressMSize(ptr, _msize);
     }
 }
